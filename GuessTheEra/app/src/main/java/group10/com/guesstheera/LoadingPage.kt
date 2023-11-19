ddc771bcea0d96ac01214b69a2b74d3c1e6c1817
shadmanhossain <shadmanh@sfu.ca>
2023-11-19 15:30:19 -0800

    Combined Login and Loading

--- GuessTheEra/app/src/main/java/group10/com/guesstheera/LoadingPage.kt
+++ GuessTheEra/app/src/main/java/group10/com/guesstheera/LoadingPage.kt
@@ -1,7 +1,10 @@
 package group10.com.guesstheera
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.TextView
@@ -9,22 +12,44 @@ import androidx.appcompat.app.AppCompatActivity
 
 class LoadingPage: AppCompatActivity()  {
     private lateinit var gameTitleTV: TextView
+    private lateinit var intent: Intent
+    private lateinit var fadeInAnimation: ObjectAnimator
+    private lateinit var fadeOutAnimation: ObjectAnimator
+    private lateinit var animatorSet: AnimatorSet
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loading_page)
         gameTitleTV = findViewById(R.id.gameTitleTV)
-        val fadeInAnimation = ObjectAnimator.ofFloat(gameTitleTV, "alpha", 0f, 1f)
-        fadeInAnimation.duration = 1000 // Set the duration of the animation in milliseconds
-        fadeInAnimation.interpolator = AccelerateDecelerateInterpolator()
+        createFadeInAnimation()
+        createFadeOutAnimation()
+        createAnimation()
+    }
 
-        val fadeOutAnimation = ObjectAnimator.ofFloat(gameTitleTV, "alpha", 1f, 0f)
+    private fun createAnimation() {
+        animatorSet = AnimatorSet()
+        animatorSet.playSequentially(fadeInAnimation, fadeOutAnimation)
+        animatorSet.start()
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                intent = Intent(this@LoadingPage, LoginActivity::class.java)
+                startActivity(intent)
+                // Finish the LoadingPage activity if you want
+                finish()
+            }
+        })
+    }
+
+    private fun createFadeOutAnimation() {
+        fadeOutAnimation = ObjectAnimator.ofFloat(gameTitleTV, "alpha", 1f, 0f)
         fadeOutAnimation.startDelay = 2000 // Wait for 2 seconds before starting fade-out
         fadeOutAnimation.duration = 1000
         fadeOutAnimation.interpolator = AccelerateDecelerateInterpolator()
+    }
 
-        val animatorSet = AnimatorSet()
-        animatorSet.playSequentially(fadeInAnimation, fadeOutAnimation)
-        animatorSet.start()
-
+    private fun createFadeInAnimation() {
+        fadeInAnimation = ObjectAnimator.ofFloat(gameTitleTV, "alpha", 0f, 1f)
+        fadeInAnimation.duration = 1000 // Set the duration of the animation in milliseconds
+        fadeInAnimation.interpolator = AccelerateDecelerateInterpolator()
     }
 }
