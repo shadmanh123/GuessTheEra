e12aa90b67bf23022f2d4d1f13640e3fa7631f20
shadmanhossain <shadmanh@sfu.ca>
2023-12-01 19:43:17 -0800

    Attempting to remove duplicate dependencies in gradle

--- GuessTheEra/app/build.gradle.kts
+++ GuessTheEra/app/build.gradle.kts
@@ -56,13 +56,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.gms:play-services-auth:19.2.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
 
 }
