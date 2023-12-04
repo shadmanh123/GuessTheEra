19cc69a14597ec9d8251f55bba95f6b5438763be
Rodrigo <rlopez@sfu.ca>
2023-12-04 01:06:34 -0800

    added photos on firebase. deleted local image resources. changed single line to account for multiple photos from same year

--- GuessTheEra/app/src/main/java/group10/com/guesstheera/GameViewModel.kt
+++ GuessTheEra/app/src/main/java/group10/com/guesstheera/GameViewModel.kt
@@ -18,73 +18,6 @@ class GameViewModel : ViewModel() {
     private val _counter = MutableLiveData<Int>()
     private var roundCounter: Int = 0
     private var isGameRunning: Boolean = false
-
-    //private var imageGameMap: Map<Int, String> = mapOf()
-    //var yearList: List<String> = listOf()
-    /*private val imageList = listOf(
-        R.drawable.p_1907,
-        R.drawable.p_1909,
-        R.drawable.p_1910,
-        R.drawable.p_1912,
-        R.drawable.p_1930,
-        R.drawable.p_1943,
-        R.drawable.p_1944,
-        R.drawable.p_1945,
-        R.drawable.p_1948,
-        R.drawable.p_1956,
-        R.drawable.p_1957,
-        R.drawable.p_1960,
-        R.drawable.p_1961,
-        R.drawable.p_1963,
-        R.drawable.p_1967,
-        R.drawable.p_1975,
-        R.drawable.p_1978,
-        R.drawable.p_1979,
-        R.drawable.p_1984,
-        R.drawable.p_1986,
-        R.drawable.p_1989,
-        R.drawable.p_1995,
-        R.drawable.p_1996,
-        R.drawable.p_2003,
-        R.drawable.p_2005,
-        R.drawable.p_2007,
-        R.drawable.p_2011,
-        R.drawable.p_2012,
-        R.drawable.p_2013,
-        R.drawable.p_2014
-    )
-    private val imageYearMap = mapOf(
-        R.drawable.p_1907 to "1907",
-        R.drawable.p_1909 to "1909",
-        R.drawable.p_1910 to "1910",
-        R.drawable.p_1912 to "1912",
-        R.drawable.p_1930 to "1930",
-        R.drawable.p_1943 to "1943",
-        R.drawable.p_1944 to "1944",
-        R.drawable.p_1945 to "1945",
-        R.drawable.p_1948 to "1948",
-        R.drawable.p_1956 to "1956",
-        R.drawable.p_1957 to "1957",
-        R.drawable.p_1960 to "1960",
-        R.drawable.p_1961 to "1961",
-        R.drawable.p_1963 to "1963",
-        R.drawable.p_1967 to "1967",
-        R.drawable.p_1975 to "1975",
-        R.drawable.p_1978 to "1978",
-        R.drawable.p_1979 to "1979",
-        R.drawable.p_1984 to "1984",
-        R.drawable.p_1986 to "1986",
-        R.drawable.p_1989 to "1989",
-        R.drawable.p_1995 to "1995",
-        R.drawable.p_1996 to "1996",
-        R.drawable.p_2003 to "2003",
-        R.drawable.p_2005 to "2005",
-        R.drawable.p_2007 to "2007",
-        R.drawable.p_2011 to "2011",
-        R.drawable.p_2012 to "2012",
-        R.drawable.p_2013 to "2013",
-        R.drawable.p_2014 to "2014"
-    )*/
     var imageArrayList: ArrayList<ByteArray>? = ArrayList()
     var imageFilePaths: List<String>? = listOf()
     private val imageDatabaseViewModel = FirebaseApplication.imageDatabaseViewModel
@@ -123,7 +56,7 @@ class GameViewModel : ViewModel() {
             }
             for (i in imageFilePaths!!.indices) {
                 val tokens: List<String> = imageFilePaths!![i].split("_")
-                val yearExtension: List<String> = tokens[1].split(".")
+                val yearExtension: List<String> = tokens[tokens.size-1].split(".")
                 yearList.add(i, yearExtension[0])
                 Log.d("debug: list size", yearList.size.toString())
             }
