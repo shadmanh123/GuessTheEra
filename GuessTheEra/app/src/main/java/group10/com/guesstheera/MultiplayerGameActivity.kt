c0a52376d7adcbb8c0e3a13961df74a6d66e8b3c
shadmanhossain <shadmanh@sfu.ca>
2023-12-05 15:34:02 -0800

    Added a space

--- GuessTheEra/app/src/main/java/group10/com/guesstheera/MultiplayerGameActivity.kt
+++ GuessTheEra/app/src/main/java/group10/com/guesstheera/MultiplayerGameActivity.kt
@@ -58,6 +58,7 @@ class MultiplayerGameActivity : AppCompatActivity() {
     private var gameId = ""
     private var winner = ""
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multiplayer_game)
